ea0b387dea023cc9d1061fceaca6c780a4a984cd
sritipraz <prajapatisriti.sp@gmail.comhttps://github.com/sritipraz/class3.git>
2017-01-09 09:51:30 +0545

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    using MyNameSpace;
+    public class Class1
+    {
+    }
+    class MyClass1
+    {
+
+    }
+    class MyClass
+    {
+
+    }
+
+}
+
+namespace MyNameSpace
+{
+    class MyClass
+    {
+        ClassLibrary1.
+    }
+}
